3e59a89108824f0c78861662d5b85696b4f9184f
damnjoejoe <kevinyuqi123@gmail.com>
2024-07-17 23:08:17 -0500

    80% finish account page

--- campaign_service/src/services/reportService.ts
+++ campaign_service/src/services/reportService.ts
@@ -1,7 +1,12 @@
 import path from "path";
 import fs from "fs";
 import datastore from "../datastore";
-import { Nullable, ReportType } from "../types/data";
+import {
+  Nullable,
+  ReportType,
+  AggregatedReports,
+  AggregatedReportsData,
+} from "../types/data";
 
 import { v4 as uuidv4 } from "uuid";
 
@@ -52,7 +57,122 @@ export const fetchReportsByCampaignId = async (
     .filter("campaignId", "=", campaignId);
 
   const [reports] = await query.run();
+
   return reports;
+};
+
+/*
+  {
+    campaignId: ReportType[]
+  }
+*/
+export const aggregateReportByCampaign = async (
+  accountId: string
+): Promise<Nullable<AggregatedReports>> => {
+  const reports = await fetchReportsByAccountId(accountId);
+  const reportsByCampaign: AggregatedReports = {
+    type: "Campaign",
+    data: {},
+  };
+
+  if (!reports) {
+    return reportsByCampaign;
+  }
+
+  const data: AggregatedReportsData = {};
+  for (let report of reports) {
+    if (!data[report.campaignId]) {
+      data[report.campaignId] = {
+        totalRevenue: 0,
+        totalCPC: 0,
+        totalClick: 0,
+        totalImpression: 0,
+        totalMoneySpent: 0,
+        campaignName: report.campaignName,
+        reports: [],
+      };
+    }
+    data[report.campaignId].reports.push(report);
+  }
+
+  /*
+    camp123: {
+      totalRevenue: 100,
+      reports: [
+        {
+          revenue: 
+        }
+      ]
+    }
+  */
+  for (let campaignId of Object.keys(data)) {
+    for (let report of data[campaignId].reports) {
+      data[campaignId].totalRevenue += parseInt(report.revenue);
+      data[campaignId].totalCPC += report.cpc;
+      data[campaignId].totalClick += parseInt(report.clickCount);
+      data[campaignId].totalImpression += parseInt(report.impCount);
+      data[campaignId].totalMoneySpent += parseInt(report.moneySpent);
+    }
+  }
+
+  reportsByCampaign.data = data;
+  return reportsByCampaign;
+};
+
+/*
+  {
+
+  }
+*/
+export const aggregateReportByAccount = async (accountId: string) => {
+  const reports = await fetchReportsByAccountId(accountId);
+  const reportsByAccount: AggregatedReports = {
+    type: "Account",
+    data: {},
+  };
+
+  if (!reports) {
+    return reportsByAccount;
+  }
+
+  const data: AggregatedReportsData = {};
+  for (let report of reports) {
+    if (!data[report.accountId]) {
+      data[report.accountId] = {
+        totalRevenue: 0,
+        totalCPC: 0,
+        totalClick: 0,
+        totalImpression: 0,
+        totalMoneySpent: 0,
+        campaignName: report.campaignName,
+        reports: [],
+      };
+    }
+    data[report.accountId].reports.push(report);
+  }
+
+  /*
+    camp123: {
+      totalRevenue: 100,
+      reports: [
+        {
+          revenue: 
+        }
+      ]
+    }
+  */
+  for (let accountId of Object.keys(data)) {
+    for (let report of data[accountId].reports) {
+      data[accountId].totalRevenue += parseInt(report.revenue);
+      data[accountId].totalCPC += report.cpc;
+      data[accountId].totalClick += parseInt(report.clickCount);
+      data[accountId].totalImpression += parseInt(report.impCount);
+      data[accountId].totalMoneySpent += parseInt(report.moneySpent);
+    }
+  }
+
+  reportsByAccount.data = data;
+  return reportsByAccount;
 };
 
 export const deleteReportById = async (reportId: string): Promise<boolean> => {
--- my-dashboard/src/layout/Dashboard.tsx
+++ my-dashboard/src/layout/Dashboard.tsx
@@ -3,26 +3,29 @@ import { useEffect, useState } from "react";
 // components
 import CardContainer from "./Card";
 import ChartContainer from "./Chart";
+import CampaignsReportTable from "../components/CampaignsReportTable";
 
 // types
-import { CardTypeKey, ReportType, ChartDataType } from "../types";
+import {
+  CardTypeKey,
+  ReportType,
+  ChartDataType,
+  CampaignType,
+  AggregatedReports,
+  CardData,
+} from "../types";
 
 import axios from "../axios";
 import { useParams } from "react-router-dom";
 
-const defaultTotal: { [key in CardTypeKey]: number } = {
-  revenue: 0,
-  cpc: 0,
-  clickCount: 0,
-  impCount: 0,
-  moneySpent: 0,
-};
-
 function Dashboard() {
-  const [reportData, setData] = useState<ReportType[]>([]);
   const [chartData, setChartData] = useState<ChartDataType | null>(null);
+  const [cardData, setCardData] = useState<CardData | null>(null);
+  const [reports, setReports] = useState<ReportType[]>([]);
 
-  const [cardData, setCardData] = useState(defaultTotal);
+  /*
+    1. get campaign id and campaign name
+  */
 
   const { accountId } = useParams();
 
@@ -30,8 +33,18 @@ function Dashboard() {
     const fetchAllReport = async () => {
       try {
         // only one platform now and all the report saved in the p1 platform
-        const response = await axios.get(`/reports/accounts/${accountId}`);
-        setData(response.data);
+        const response = await axios.get<AggregatedReports>(
+          `/reports/accounts/${accountId}`
+        );
+        const data = response.data.data[`${accountId}`] || {};
+        setReports(data.reports || []);
+        setCardData({
+          revenue: data.totalRevenue,
+          clickCount: data.totalClick,
+          cpc: data.totalCPC,
+          impCount: data.totalImpression,
+          moneySpent: data.totalMoneySpent,
+        });
       } catch (error) {
         console.error("FETCH_REPORT_PLATFORM_ERROR: ", error);
       }
@@ -42,29 +55,10 @@ function Dashboard() {
     }
   }, [accountId]);
 
-  useEffect(() => {
-    if (reportData && reportData.length) {
-      let updatedTotal: { [key in CardTypeKey]: number } = { ...defaultTotal };
-
-      // loop reportData
-      for (let report of reportData) {
-        // loop each key of total {revenue: 0, cpc: 0, ...} and update the value
-        for (let key in updatedTotal) {
-          updatedTotal[key as CardTypeKey] +=
-            typeof report[key as CardTypeKey] === "string"
-              ? parseInt(report[key as CardTypeKey] as string, 10)
-              : (report[key as CardTypeKey] as number);
-        }
-      }
-
-      setCardData(updatedTotal);
-    }
-  }, [reportData]);
-
   const selectCard = (key: CardTypeKey) => {
     const values: number[] = [];
     const dates: string[] = [];
-    for (let report of reportData) {
+    for (let report of reports) {
       if (typeof report[key] === "string") {
         values.push(parseInt(report[key] as string, 10) as number);
       } else if (typeof report[key] === "number") {
@@ -90,6 +84,8 @@ function Dashboard() {
       list of revenue, cpc, clickCount
       */}
       {chartData && <ChartContainer data={chartData} />}
+      {/* Campaigns Table */}
+      {accountId && <CampaignsReportTable accountId={accountId} />}
     </div>
   );
 }
